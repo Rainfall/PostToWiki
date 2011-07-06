9c542976e73ccfd257dc1857b6682e8814eda28f
Denis <paradoxetion@gmail.com>
2011-07-06 19:57:21 +0400

    first Project commit

--- PycharmProjects/PostToWiki/Main.py
+++ PycharmProjects/PostToWiki/Main.py
@@ -1,3 +1,5 @@
+# coding=utf-8>
+from xmlrpclib import Server
 
 import ldap
 
@@ -37,22 +39,53 @@ if __name__ == "__main__":
 #  L = ldapStorage(base='dc=griddynamics,dc=net')
 #  print L.search_user('akornev')
 
-  def denis_search(host, attr, n):
+  def list_name_group(host, group):
       open=ldap.open(host)
-      search= open.search(base='ou=people,ou=griddynamics,dc=griddynamics,dc=net', scope=ldap.SCOPE_SUBTREE,
-                       attrlist=[attr])
-      result = open.result(msgid=search)[1][n][1]['cn'][0]
+      result = []
+
+      filter='cn=%s' % group
+
+      search_name = open.search(base='ou=groups,dc=griddynamics,dc=net', scope=ldap.SCOPE_SUBTREE,
+                      filterstr=filter, attrlist=['member'])
+
+      result_name = open.result(msgid=search_name)[1][0][1]['member']
+
+      for i in range(0,len(result_name),1):
+
+           search_people = open.search(base=result_name[i], scope=ldap.SCOPE_BASE, attrlist=['uid', 'cn'])
+           result_people = open.result(msgid=search_people)
+
+           result.append({"ID": result_people[1][0][1]['uid'][0], "Name":result_people[1][0][1]['cn'][0]})
+
       return result
 
+  wikiURL = 'https://payu-wiki.carina.griddynamics.net:8443/display/~'
   host = 'ns-carina.carina.griddynamics.net'
-  attr = 'cn'
-  n=1
+  group = 'microsoft-management'
 
-  while True:
-      try:
-        L2search = denis_search(host,attr,n)
-        print L2search
-        n+=1
-      except :
-          break
 
+  L2search = list_name_group(host, group)
+  print L2search
+  L2search_for_posting=''
+  userLink = ''
+  for i in L2search:
+      userLink = wikiURL+i["ID"]
+      L2search_for_posting += '* ['+i['Name']+'|'+userLink+']\n'
+  print L2search_for_posting
+
+  import SOAPpy
+  from SOAPpy import Types
+
+  url = 'https://payu-wiki.carina.griddynamics.net:8443/rpc/soap-axis/confluenceservice-v1?wsdl'
+  soap = SOAPpy.WSDL.Proxy(url)
+  user='student'
+  passwd='Aeju1lu6'
+  spaceKey = 'STD'
+  auth = soap.login(user, passwd)
+  
+  space = soap.getSpace(auth, spaceKey)
+
+  soap.storePage(auth, {"space":spaceKey, "title": group, "content": L2search_for_posting})
+
+  logout = soap.logout(auth)
+  print logout
